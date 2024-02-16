29d3957a332315520adaa8ccb6f5c1eff18d736c
Luiz Pachioni <lopachioni@gmail.com>
2024-02-15 22:43:06 -0300

    ajustando a base url

--- src/services/AxiosClient.ts
+++ src/services/AxiosClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'https://localhost:7132/',
+    baseURL: 'http://ec2-3-144-24-58.us-east-2.compute.amazonaws.com:5131/',
 });
 
 export default axiosClient;
